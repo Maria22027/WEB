eb012ffe9b750c33d7693bcbee25c855f07a3f6b
Maria Eduarda <101368201+Maria22027@users.noreply.github.com>
2024-12-06 14:20:43 -0300

    Projeto Django
    Projeto desenvolvido a grosso modo

---
+++ Projeto_Django-main/app/urls.py
@@ -0,0 +1,39 @@
+#importa a função path do modulo django.urls para definir os padrões de URL
+from django.urls import path
+
+#importe o módulo views do diretório atual
+from . import views
+
+#lista de objetos path, define padrões de URL
+urlpatterns = [
+    #define a view a ser chamada quando a URL é acessada
+    
+    path('' , views.app, name="app"),
+    path('login/' , views.fazerlogin, name="fazerlogin"),
+    path('cadastro/' , views.cadastrar_user, name="cadastrar_user"),
+    path('usuarios/' , views.exibir_user, name="exibir_user"),
+    path('cursos/' , views.exibir_curso, name="exibir_curso"),
+    path('cadastro_curso/' , views.cadastrar_curso, name="cadastrar_curso"),
+    
+    #editar e excluir
+    path('editar_usuario/<int:id_usuario>' , views.editar_usuario, name="editar_usuario"),
+    path('excluir_usuario/<int:id_usuario>' , views.excluir_usuario, name="excluir_usuario"),
+
+    #dashboard
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('alterar_senha/', views.alterar_senha, name='alterar_senha'),
+    path('excluir_conta/', views.excluir_conta, name='excluir_conta'),
+    path('logout/', views.userLogout, name='logout'),
+
+
+    #foto
+    path('add-foto/', views.add_foto, name='add_foto'),
+    path('galeria/', views.galeria, name='galeria'),
+
+    #projetos
+    path('excluir_curso/<int:id_curso>', views.excluir_curso, name='excluir_curso'),
+
+    #contato
+    path('contato', views.contato, name='contato'),
+
+]
---
+++ Projeto_Django-main/app/views.py
@@ -0,0 +1,241 @@
+from datetime import timedelta
+from django.shortcuts import render,redirect
+
+#importa a funcao get_template() do módulo loader
+from app.forms import FormCadastroUser, FormCadastroCurso, FormLogin, FormFoto, FormContato
+from django.contrib import messages
+from app.models import Usuario, Curso, Foto
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth import logout
+
+def app(request):
+    
+    email_do_usuario = request.session.get('email')
+
+    context = {
+        'email_do_usuario': email_do_usuario,
+     }
+    return render(request, 'index.html', context)
+
+
+def cadastrar_user(request):
+    novo_user = FormCadastroUser(request.POST or None, request.FILES or None)
+    
+    # SALVAR USUÁRIO
+    if request.method == 'POST':
+        if novo_user.is_valid():
+            usuario = novo_user.save(commit=False)  # Evita salvar imediatamente no banco
+            # Criptografa a senha antes de salvar
+            usuario.senha = make_password(novo_user.cleaned_data['senha'])
+            usuario.save()  # Agora salva com a senha criptografada
+            messages.success(request, "Usuário cadastrado com sucesso!")
+            return redirect('app')  # Redireciona para a página principal ou de login
+    
+    context = {
+        'form': novo_user
+    }
+
+    return render(request, 'cadastro.html', context)
+
+def cadastrar_curso(request):
+    if not request.session.get('email'):
+        messages.error(request, "Faça seu login para cadastrar seu portfólio!")
+        return redirect('fazerlogin')
+    
+    novo_curso = FormCadastroCurso(request.POST or None, request.FILES or None)
+    #SALVAR USUÁRIO
+    if request.POST:
+        if novo_curso. is_valid():
+            novo_curso.save()
+            messages.success(request, "Curso Cadastrado com sucesso!")
+            return redirect('app')
+    context = {
+        'form' : novo_curso
+    }
+
+    return render(request, 'cadastro_curso.html', context)
+
+def exibir_user(request):
+    usuarios = Usuario.objects.all()
+
+    context = {
+        'dados' : usuarios
+    }
+
+    return render(request, 'usuarios.html', context)
+
+def exibir_curso(request):
+    cursos = Curso.objects.all()
+
+    context = {
+        'cursos' : cursos
+    }
+
+    return render(request, 'cursos.html', context)
+
+def dashboard(request):
+    if not request.session.get('email'):
+        return redirect('fazerlogin')
+
+    email = request.session['email']
+    usuario = Usuario.objects.get(email=email)
+
+    context = {
+        'usuario': usuario
+    }
+    
+    return render(request, 'dashboard.html', context)
+
+def fazerlogin(request):
+    formL = FormLogin(request.POST or None)
+    
+    if request.method == 'POST':       
+        _email = request.POST.get('email')
+        _senha = request.POST.get('senha')
+        
+        if not _email or not _senha:
+            messages.error(request, 'Por favor, preencha todos os campos.')
+            return render(request, 'login.html', {'formLogin': formL})
+
+        try:
+            usuarioL = Usuario.objects.get(email=_email)
+            if check_password(_senha, usuarioL.senha):  
+                request.session.set_expiry(30)  # Sessão de 30 segundos
+                request.session['email'] = _email
+                messages.success(request, 'Login bem-sucedido!')
+                return redirect('dashboard')  # Redireciona para o dashboard
+            else:
+                messages.error(request, 'Credenciais inválidas. Por favor, tente novamente!')
+        except Usuario.DoesNotExist:
+            messages.error(request, 'Credenciais inválidas. Por favor, tente novamente!')
+    
+    return render(request, 'login.html', {'formLogin': formL})
+
+def editar_usuario(request, id_usuario):
+    if not request.session.get('email'):
+        messages.error(request, "Faça seu login para Editar ou Excluir contas!")
+        return redirect('fazerlogin')
+
+    usuario = Usuario.objects.get(id=id_usuario)
+    form = FormCadastroUser(request.POST or None, instance=usuario)
+    if request.POST:
+        if form.is_valid():
+            form.save()
+            return redirect('exibir_user')
+    context = {
+            'form' : form
+        }
+    return render(request, 'editar_usuario.html', context)
+
+def excluir_usuario(request, id_usuario):
+    if not request.session.get('email'):
+        messages.error(request, "Faça seu login para Editar ou Excluir contas!")
+        return redirect('fazerlogin')
+
+    usuario = Usuario.objects.get(id=id_usuario)
+    usuario.delete()
+    return redirect('exibir_user')
+
+def excluir_conta(request):
+    if not request.session.get('email'):
+        return redirect('fazerlogin')
+    
+    email = request.session['email']
+    usuario = Usuario.objects.get(email=email)
+
+    # Exclui o usuário
+    usuario.delete()
+    
+    # Limpa a sessão do usuário
+    request.session.flush()  
+    
+    # Redireciona para a página inicial
+    messages.success(request, 'Conta excluída com sucesso.')
+    return redirect('app')  # Ou o nome da sua página inicial
+
+def alterar_senha(request):
+    if not request.session.get('email'):
+        return redirect('fazerlogin')
+
+    if request.method == 'POST':
+        email = request.session['email']
+        usuario = Usuario.objects.get(email=email)
+        
+        senha_atual = request.POST.get('senha_atual')
+        nova_senha = request.POST.get('nova_senha')
+        confirmacao_senha = request.POST.get('confirmacao_senha')
+
+        # Verifica se a senha atual está correta
+        if not check_password(senha_atual, usuario.senha):
+            messages.error(request, 'A senha atual está incorreta.')
+            return render(request, 'alterar_senha.html')
+
+        # Verifica se a nova senha e a confirmação coincidem
+        if nova_senha != confirmacao_senha:
+            messages.error(request, 'As senhas não coincidem.')
+            return render(request, 'alterar_senha.html')
+
+        # Atualiza a senha
+        usuario.senha = make_password(nova_senha)  # Criptografa a nova senha
+        usuario.save()
+        messages.success(request, 'Senha alterada com sucesso!')
+        return redirect('dashboard')
+
+    return render(request, 'alterar_senha.html')
+
+def add_foto(request):
+    if request.method == 'POST':
+        form = FormFoto(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('galeria')
+    else:
+        form = FormFoto()
+    return render (request, 'add_foto.html', {'form': form})
+
+def galeria(request):
+    # Buscando as fotos de cada modelo
+    usuarios_fotos = Usuario.objects.all()
+    cursos_fotos = Curso.objects.all()
+    fotos_fotos = Foto.objects.all()
+
+    # Passando as fotos para o contexto
+    context = {
+        'usuarios_fotos': usuarios_fotos,
+        'cursos_fotos': cursos_fotos,
+        'fotos_fotos': fotos_fotos,
+    }
+    
+    return render(request, 'galeria.html', context)
+
+def excluir_curso(request, id_curso):
+    curso = Curso.objects.get(id=id_curso)
+    curso.delete()
+    return redirect('exibir_curso')
+
+def contato(request):
+    novo_contato = FormContato(request.POST or None)
+    
+    # SALVAR USUÁRIO
+    if request.method == 'POST':
+        if novo_contato.is_valid():
+            contato = novo_contato.save(commit=True)
+            messages.success(request, "Mensagem enviada com sucesso!")
+            return redirect('app')  # Redireciona para a página principal ou de login
+    
+    context = {
+        'form': novo_contato
+    }
+
+    return render(request, 'contato.html', context)
+
+def venda(request):
+    if not request.session.get('email'):
+        messages.error(request, "Faça seu login para Efetuar a compra!")
+        return redirect('fazerlogin')
+
+
+def userLogout (request):
+    logout(request)
+    messages.success(request, 'A sessao foi encerrada!')
+    return redirect('app')
